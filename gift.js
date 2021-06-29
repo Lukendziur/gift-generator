6683fadf6d665e6e943ac8598cfd2281b5713946
LuKendziur <lukendziur@hotmail.com>
2021-06-29 11:53:52 -0300

    loaded files

---
+++ gift.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', () => {
+    async function giftFunction() {
+        const data = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=F7NNx7J2Q6taRLVi9TmWBpEPJqMDSjJV`)
+        const json = await data.json();
+
+        const div = document.querySelector('div')
+        const button = document.querySelector('button')
+
+
+        function random_item(items) {
+
+            return items[Math.floor(Math.random() * items.length)];
+
+        }
+
+
+
+        button.addEventListener('click', () => {
+            if (button) {
+
+                let div = document.querySelector('div')
+                div.classList.add('container')
+                let random = random_item(json.data)
+                let h3ToInsert = document.querySelector('h3')
+                let imgToInsert = document.querySelector('figcaption')
+                h3ToInsert.innerHTML = ` ${random.title}`
+                imgToInsert.innerHTML = `<img src=${random.images.original.url}>`
+
+
+            }
+
+
+        })
+
+
+
+    }
+    giftFunction();
+
+
+
+
+
+})
